4c80f33f89446192cc0983d74b7fd27946500d00
Raystin <icarokart@hotmail.com>
2022-09-23 13:53:22 -0300

    001 - Arquivos iniciais

---
+++ Projeto Site/Views/Home/Class.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Site.Views.Home
+{
+    public class Class
+    {
+    }
+}
